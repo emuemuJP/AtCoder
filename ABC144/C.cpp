1437cc5662e269cc45f219783001631d389de15c
emuemuJP <k.matsumoto.0807@gmail.com>
2020-04-21 10:22:49 +0900

    solve C

---
+++ ABC144/C.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <algorithm>
+#include <cmath>
+#include <bitset>
+#include <iomanip>
+#include <stack>
+#include <list>
+#include <map>
+#include <unordered_map>
+#include <chrono>
+#include <numeric>
+using namespace std;
+using ll = long long;
+const long long INF = (long long)1e18+1;
+#define DIV 1000000007
+int main()
+{
+    cin.tie(0);
+    ios::sync_with_stdio(false);
+#ifdef TEST
+    chrono::system_clock::time_point start, end;
+    start = chrono::system_clock::now();
+#endif
+    long idx=0;
+    long N;
+    cin >> N;
+    ll max_div = 0;
+    for(size_t i=1;i*i<=N; i++)
+    {
+        if(N % i == 0) max_div = i;
+    }
+    cout << (max_div - 1) + (N / max_div - 1) << endl;
+#ifdef TEST
+    end = chrono::system_clock::now();
+    cerr << static_cast<double>(chrono::duration_cast<chrono::microseconds>(end - start).count() / 1000.0) << "[ms]" << endl;
+#endif
+    return 0;
+}
