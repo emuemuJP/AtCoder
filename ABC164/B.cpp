06140d799d1dd505c8cafffb734208efb8721b51
emuemuJP <k.matsumoto.0807@gmail.com>
2020-04-26 21:59:54 +0900

    solve A B C

---
+++ ABC164/B.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <algorithm>
+#include <cmath>
+#include <bitset>
+#include <iomanip>
+#include <stack>
+#include <list>
+#include <map>
+#include <unordered_map>
+#include <chrono>
+#include <numeric>
+using namespace std;
+using ll = long long;
+const ll INF = (ll)1e18+1;
+const ll DIV = 1000000007;
+//#define TEST
+int main()
+{
+    cin.tie(0);
+    ios::sync_with_stdio(false);
+#ifdef TEST
+    chrono::system_clock::time_point start, end;
+    start = chrono::system_clock::now();
+#endif
+    long A, B, C, D;
+    cin >> A >> B >> C >> D;
+    ll takahashi_count = C / B, aoki_count = A / D;
+    if(C % B > 0) takahashi_count++;
+    if(A % D > 0) aoki_count++;
+    if(takahashi_count <= aoki_count) cout << "Yes" << endl;
+    else cout << "No" << endl;
+#ifdef TEST
+    end = chrono::system_clock::now();
+    cerr << static_cast<double>(chrono::duration_cast<chrono::microseconds>(end - start).count() / 1000.0) << "[ms]" << endl;
+#endif
+    return 0;
+}
