c84eced9517dd47cd0ff707fc93cc4d3eff5b386
emuemuJP <k.matsumoto.0807@gmail.com>
2020-05-02 23:00:39 +0900

    solve D

---
+++ ABC165/D.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <algorithm>
+#include <cmath>
+#include <bitset>
+#include <iomanip>
+#include <stack>
+#include <list>
+#include <map>
+#include <unordered_map>
+#include <chrono>
+#include <numeric>
+using namespace std;
+using ll = long long;
+const ll INF = (ll)1e18+1;
+const ll DIV = 1000000007;
+//#define TEST
+int main()
+{
+    cin.tie(0);
+    ios::sync_with_stdio(false);
+#ifdef TEST
+    chrono::system_clock::time_point start, end;
+    start = chrono::system_clock::now();
+#endif
+    ll A, B, N;
+    cin >> A >> B >> N;
+    ll max_num = 0;
+    ll x = min(B-1, N);
+    ll ax_b = (A * x)/B;
+    ll x_b = x / B;
+    cout << ax_b - A * x_b << endl;
+#ifdef TEST
+    end = chrono::system_clock::now();
+    cerr << static_cast<double>(chrono::duration_cast<chrono::microseconds>(end - start).count() / 1000.0) << "[ms]" << endl;
+#endif
+    return 0;
+}
