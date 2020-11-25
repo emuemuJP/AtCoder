ff10ee1ffb07ecee368c528fda51fb0887bcafec
emuemuJP <k.matsumoto.0807@gmail.com>
2020-11-25 10:40:13 +0900

    solve B

---
+++ ABC105/B.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <algorithm>
+#include <cmath>
+#include <bitset>
+#include <iomanip>
+#include <stack>
+#include <list>
+#include <map>
+#include <unordered_map>
+#include <chrono>
+#include <numeric>
+using namespace std;
+using ll = long long;
+const ll INF = (ll)1e18+1;
+const ll DIV = 1000000007;
+//#define TEST
+int main()
+{
+    cin.tie(0);
+    ios::sync_with_stdio(false);
+#ifdef TEST
+    chrono::system_clock::time_point start, end;
+    start = chrono::system_clock::now();
+#endif
+    long N;
+    cin >> N;
+    for(size_t i=0;i<=N / 7; i++)
+    {
+        for(size_t j=0;j<=(N - 7*i)/4; j++)
+        {
+            //cerr << i << ":" << j << endl;
+            if(N == i * 7  + j * 4 )
+            {
+                cout << "Yes" << endl;
+                return 0;
+            }
+        }
+    }
+    cout << "No" << endl;
+#ifdef TEST
+    end = chrono::system_clock::now();
+    cerr << static_cast<double>(chrono::duration_cast<chrono::microseconds>(end - start).count() / 1000.0) << "[ms]" << endl;
+#endif
+    return 0;
+}
