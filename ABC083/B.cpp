326970d0ef6a20608a4882e8e3e1d7c38ec92a9e
emuemuJP <k.matsumoto.0807@gmail.com>
2020-12-17 18:29:19 +0900

    solve B

---
+++ ABC083/B.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <algorithm>
+#include <cmath>
+#include <bitset>
+#include <iomanip>
+#include <stack>
+#include <list>
+#include <map>
+#include <unordered_map>
+#include <chrono>
+#include <numeric>
+using namespace std;
+using ll = long long;
+const ll INF = (ll)1e18+1;
+const ll DIV = 1000000007;
+//#define TEST
+ll digit_sum(ll n) {
+    ll res = 0;
+    while(n > 0) {
+        res += n%10;
+        n /= 10;
+    }
+    return res;
+}
+
+int main()
+{
+    cin.tie(0);
+    ios::sync_with_stdio(false);
+#ifdef TEST
+    chrono::system_clock::time_point start, end;
+    start = chrono::system_clock::now();
+#endif
+    long N,A,B;
+    cin >> N >> A >> B;
+    ll total=0;
+    for(ll i=1;i<=N; i++)
+    {
+        ll d_sum = digit_sum(i);
+        if(d_sum >= A && d_sum <=B) total+=i;
+    }
+    cout << total << endl;
+#ifdef TEST
+    end = chrono::system_clock::now();
+    cerr << static_cast<double>(chrono::duration_cast<chrono::microseconds>(end - start).count() / 1000.0) << "[ms]" << endl;
+#endif
+    return 0;
+}
