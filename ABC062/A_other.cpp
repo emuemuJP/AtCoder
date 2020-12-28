015cac221a8fb8035a6879554ae9f2b353aaabca
emuemuJP <k.matsumoto.0807@gmail.com>
2020-12-28 20:54:53 +0900

    solve A other

---
+++ ABC062/A_other.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <algorithm>
+#include <cmath>
+#include <bitset>
+#include <iomanip>
+#include <stack>
+#include <list>
+#include <map>
+#include <unordered_map>
+#include <chrono>
+#include <numeric>
+using namespace std;
+using ll = long long;
+const ll INF = (ll)1e18+1;
+const ll DIV = 1000000007;
+//#define TEST
+int main()
+{
+    cin.tie(0);
+    ios::sync_with_stdio(false);
+#ifdef TEST
+    chrono::system_clock::time_point start, end;
+    start = chrono::system_clock::now();
+#endif
+    long x,y;
+    cin >> x >> y;
+    vector<long> a = {1,3,1,2,1,2,1,1,2,1,2,1};
+    cout << ((a[x-1] == a[y-1]) ? "Yes" : "No") << endl;
+#ifdef TEST
+    end = chrono::system_clock::now();
+    cerr << static_cast<double>(chrono::duration_cast<chrono::microseconds>(end - start).count() / 1000.0) << "[ms]" << endl;
+#endif
+    return 0;
+}
