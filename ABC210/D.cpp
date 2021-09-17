ccc8485d64486ef0baf2a9f013ae77ae567bc75f
emuemuJP <k.matsumoto.0807@gmail.com>
2021-09-18 00:00:21 +0900

    2マスの和最小値を考える場合でかつ位置関係が重要なとき、位置関係べつに動的計画法で最小値を求め、最後最小値を求める。この時計算量は行ｘ列の定数倍で済む。

--- ABC210/D.cpp
+++ ABC210/D.cpp
@@ -37,37 +37,60 @@ int main()
 #endif
     ll H,W,C;
     cin >> H >> W >> C;
-    std::vector<std::vector<ll>> A(H, vector<ll>(W, 0));
-    vector<ll> B(H*W);
-    for(size_t i=0;i<H; i++)
-    {
-        for(size_t j=0;j<W; j++)
-        {
-            cin >> A[i][j];
-            cin >> B[i*W + j];
-        }
+    vector<vector<ll>> A(H, vector<ll>(W)), RD(H, vector<ll>(W, INF)), LD(H, vector<ll>(W, INF));
+    for(auto& a : A) for(ll& i : a) cin >> i;
+    //駅の位置関係が ＼ となる場合をサーチ
+    for(ll i = 0; i < H; i++) for(ll j = 0; j < W; j++){
+        //そこで駅を立てるのが安いか、そこに至るまででより安い建て方があるか？
+        //どちらにせよ移動費はかかるのでCを足す
+        const ll x = min(A[i][j], RD[i][j]) + C;
+        //探索可能ならサーチ
+        if(j + 1 < W) chmin(RD[i][j + 1], x);
+        if(i + 1 < H) chmin(RD[i + 1][j], x);
     }
-    ll cost = INF;
-    for(size_t i=0;i<H*W; i++)
-    {
-        ll h_i = i / W;
-        ll w_i = i % W;
-        ll dist = (cost -  A[h_i][w_i]) / C;
-        if((cost -  A[h_i][w_i]) % C >0) dist++;
-
-        for(ll h_j=h_i;h_j<min(h_i+dist, H); h_j++)
-        {
-            ll dist_h = abs(h_j - h_i);
-            for(ll w_j=max(w_i - (dist - dist_h), 0ll);w_j<max(min(w_i + (dist - dist_h), W), W); w_j++)
-            {
-                if(h_j==h_i && w_j <= w_i) continue;
-                ll dist_w = abs(w_j - w_i);
-                chmin(cost, A[h_i][w_i] + A[h_j][w_j]+C*(dist_h+dist_w));
-            }
-        }
-
+    //駅の位置関係が ／ となる場合をサーチ
+    for(ll i = 0; i < H; i++) for(ll j = W; j--; ){
+        const ll x = min(A[i][j], LD[i][j]) + C;
+        if(j) chmin(LD[i][j - 1], x);
+        if(i + 1 < H) chmin(LD[i + 1][j], x);
     }
-    cout << cost << endl;
+    ll ans = INF;
+    for(ll i = 0; i < H; i++) for(ll j = 0; j < W; j++)
+    {
+        chmin(ans, A[i][j] + min(RD[i][j], LD[i][j]));
+        //cerr << RD[i][j] << ":" << LD[i][j] << endl;
+    }
+    cout << ans << endl;
+    // std::vector<std::vector<ll>> A(H, vector<ll>(W, 0));
+    // vector<ll> A_(H*W);
+    // for(size_t i=0;i<H; i++)
+    // {
+    //     for(size_t j=0;j<W; j++)
+    //     {
+    //         cin >> A[i][j];
+    //         A_[j + i*W] = A[i][j];
+    //         //cerr << i*W + j << endl;
+    //     }
+    // }
+    // ll cost = INF;
+    // for(size_t i=0;i<H*W; i++)
+    // {
+    //     ll h_i = i / W;
+    //     ll w_i = i % W;
+    //     //cerr << A_[i] << endl;
+    //     if(A_[i] > cost) continue;
+    //     ll max_dist = (cost - A_[i]) / C;
+    //     for(size_t j=i+1;j<H*W; j++)
+    //     {
+    //         ll h_j = j / W;
+    //         ll w_j = j % W;
+    //         ll dist = abs(h_j - h_i) + abs(w_j - w_i);
+    //         if(dist > max_dist) continue;
+    //         chmin(cost, A_[i] +  A_[j] + C * dist);
+    //         //cerr << h_j << ":" << w_j << ":" << A_[j] << endl;
+    //     }
+    // }
+    // cout << cost << endl;
 #ifdef TEST
     end = chrono::system_clock::now();
     cerr << static_cast<double>(chrono::duration_cast<chrono::microseconds>(end - start).count() / 1000.0) << "[ms]" << endl;
